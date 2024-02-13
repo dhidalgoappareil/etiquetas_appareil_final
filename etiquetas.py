6f03ba73c7e7f045256df10bca2d220385497fc2
dhidalgoappareil <159042017+dhidalgoappareil@users.noreply.github.com>
2024-02-13 10:47:10 -0300

    etiquetas.py

--- etiquetas.py
+++ etiquetas.py
@@ -1,10 +1,10 @@
 import sys
 import shutil
+import os
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QFileDialog
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.units import inch
 from reportlab.lib import colors
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
@@ -22,7 +22,7 @@ class DataInputWidget(QWidget):
         """)
 
         # Preguntar por el tipo de cliente (Particular o Privado)
-        self.client_type_label = QLabel('<b>¿Es el cliente Particular o Privado?</b>')
+        self.client_type_label = QLabel('<b>¿Es el cliente <font color=black>Particular</font> o <font color=black>Privado</font>?</b>')
         self.client_type_label.setStyleSheet("font-size: 16px;")
         self.client_type_label.setWordWrap(True)
 
@@ -68,7 +68,7 @@ class DataInputWidget(QWidget):
         # Constante de porcentaje de escala de la imagen
         self.scale_percent = 62
 
-        # Botones de generación, guardar e imprimir
+        # Botones de generación y guardar
         self.submit_button = QPushButton('Generar PDF')
         self.submit_button.setStyleSheet("""
             background-color: #4CAF50;
@@ -77,10 +77,8 @@ class DataInputWidget(QWidget):
             padding: 10px 24px;
             text-align: center;
             text-decoration: none;
-            display: inline-block;
             font-size: 16px;
             margin: 4px 2px;
-            cursor: pointer;
             border-radius: 8px;
         """)
         self.save_button = QPushButton('Guardar PDF')
@@ -91,24 +89,8 @@ class DataInputWidget(QWidget):
             padding: 10px 24px;
             text-align: center;
             text-decoration: none;
-            display: inline-block;
             font-size: 16px;
             margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 8px;
-        """)
-        self.print_button = QPushButton('Imprimir PDF')
-        self.print_button.setStyleSheet("""
-            background-color: #f44336;
-            border: none;
-            color: white;
-            padding: 10px 24px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
             border-radius: 8px;
         """)
 
@@ -130,16 +112,17 @@ class DataInputWidget(QWidget):
 
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.submit_button)
+
+
         button_layout.addWidget(self.save_button)
-        button_layout.addWidget(self.print_button)
 
         layout.addLayout(button_layout)
 
         self.submit_button.clicked.connect(self.generate_pdf)
         self.save_button.clicked.connect(self.save_pdf)
-        self.print_button.clicked.connect(self.print_pdf)
         self.setLayout(layout)
         self.pdf_counter = 1  # Inicializamos el contador de PDFs
+        self.output_directory = r'C:\Users\mcorr\OneDrive\Desktop\Programas\etiquetas_appareil'  # Inicializa output_directory
 
     def generate_pdf(self):
         # Obtener los datos ingresados por el usuario
@@ -151,8 +134,8 @@ class DataInputWidget(QWidget):
         email = self.email_input.text()
         invoice_number = self.invoice_number_input.text()
 
-        # Crear el contenido del PDF
-        pdf_filename = f'cliente_info_{self.pdf_counter}.pdf'
+        # Crea el nombre del archivo PDF con el directorio especificado usando os.path.join
+        pdf_filename = os.path.join(self.output_directory, f'cliente_info_{self.pdf_counter}.pdf')
         doc = SimpleDocTemplate(pdf_filename, pagesize=letter)
         styles = getSampleStyleSheet()
 
@@ -168,43 +151,45 @@ class DataInputWidget(QWidget):
             alignment=1  # 0=Left, 1=Center, 2=Right
         )
 
-        title_style = ParagraphStyle(
-            name='TitleStyle',
+        custom_title_style = ParagraphStyle(
+            name='CustomTitleStyle',
             parent=styles['Normal'],
-            fontName='Calibri',
-            fontSize=24,
+            fontName='Times-Bold',  # Cambiar la fuente a Times New Roman en negritas
+            fontSize=28,  # Cambiar el tamaño de la fuente a 28
             alignment=1,
             textColor=colors.black,
             spaceAfter=24
         )
 
-        bold_style = ParagraphStyle(
-            name='BoldStyle',
-            parent=styles['Normal'],
-            fontName='Calibri',
-            fontSize=16,
-            textColor=colors.black,
-            spaceAfter=12
+        custom_title_style2 = ParagraphStyle(
+            name='CustomTitleStyle2',  # Nombre del estilo, utilizado para referencia.
+            parent=styles['Normal'],  # Estilo base del cual hereda características.
+            fontName='Times-Bold',   # Nombre de la fuente (Times New Roman en negritas).
+            fontSize=18,  # Tamaño de la fuente en puntos. Cambiar la fuente en 18.
+            alignment=1,  # Alineación del texto (0=izquierda, 1=centro, 2=derecha).
+            textColor=colors.black,   # Color del texto.
+            spaceAfter=2   # Espacio después del párrafo.
         )
+
 
         content = []
 
         # Agregar título al PDF
-        content.append(Paragraph('<b>ETIQUETA DE DATOS</b>', title_style))
+        content.append(Paragraph('<b>ETIQUETA DE DATOS</b>', custom_title_style))
 
         # Agregar contenido al PDF con saltos de línea
         content.append(Spacer(1, 24))
-        content.append(Paragraph(f'<b>{client_type}</b>', centered_style))
+        content.append(Paragraph(f'<b><font color=black>{client_type}</font></b>', custom_title_style2))
         content.append(Paragraph(f'{company_location}', centered_style))
         content.append(Paragraph(f'{rut}', centered_style))
-        content.append(Paragraph(f'<b>{client_name}</b>', centered_style))
-        content.append(Paragraph(f'{contact_phone}\n{email}', centered_style))
+        content.append(Paragraph(f'<b>{client_name}</b>', custom_title_style2))
+        content.append(Paragraph(f'{contact_phone}\n {email}', centered_style))
         content.append(Spacer(1, 24))
         content.append(Paragraph('<b>FACTURA</b>', centered_style))
-        content.append(Paragraph(f'<b>N° {invoice_number}</b>', centered_style))
+        content.append(Paragraph(f'<b>N° {invoice_number}</b>', custom_title_style2))
 
         # Agregar la imagen al final del PDF y escalarla
-        img_path = 'C:/Users/Appareil/OneDrive/Escritorio/Programa/imagen_HD_pie.png'
+        img_path = 'C:/Users/mcorr/OneDrive/Desktop/Programas/etiquetas_appareil/imagen_HD_pie.png'
         img = Image(img_path)
         img.drawWidth = img.drawWidth * self.scale_percent / 100  # Escalar el ancho
         img.drawHeight = img.drawHeight * self.scale_percent / 100  # Escalar la altura
@@ -220,16 +205,12 @@ class DataInputWidget(QWidget):
         file_path, _ = QFileDialog.getSaveFileName(self, "Guardar PDF", "", "Archivos PDF (*.pdf)")
         if file_path:
             # Obtener el nombre del archivo PDF generado
-            pdf_filename = f'cliente_info_{self.pdf_counter - 1}.pdf'
-            
+            pdf_filename = os.path.join(self.output_directory, f'cliente_info_{self.pdf_counter - 1}.pdf')
+
             # Copiar el PDF generado al destino especificado
             shutil.copy(pdf_filename, file_path)
-            
-            QMessageBox.information(self, "PDF Guardado", f'El PDF se ha guardado en: {file_path}')
 
-    def print_pdf(self):
-        self.generate_pdf()  # Generar el PDF primero
-        QMessageBox.information(self, "PDF Impreso", "El PDF se ha enviado a la impresora.")
+            QMessageBox.information(self, "PDF Guardado", f'El PDF se ha guardado en: {file_path}')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
